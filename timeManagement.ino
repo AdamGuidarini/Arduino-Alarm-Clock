9048ca0c1b9890e9e1b178fee9c36dfd2ac176cd
AdamG95 <45023561+AdamG95@users.noreply.github.com>
2021-04-30 17:53:06 -0500

    Update timeManagement.ino

--- timeManagement.ino
+++ timeManagement.ino
@@ -13,18 +13,19 @@ void changeTime()
 {
   wipeBottom();
   seconds = 0;
-
+  printTime(hours, minutes, seconds, amPM);
+  
   // Update Hours
-  changeHour();
-  delay(200);
+  hours = changeHour(hours);
+  delay(300);
 
   //Update Minutes
-  changeMinute();
-  delay(200);
+  minutes = changeMinute(minutes);
+  delay(300);
 
   // Update AM/PM
   changeAmPm();
-  delay(200);
+  delay(300);
 
   wipeBottom();
   
@@ -35,75 +36,79 @@ void changeTime()
 /**
  * Allows user to change hour with buttons
  ********************************************************************************/
-void changeHour()
+unsigned int changeHour(unsigned int hour)
 {
   changeHourMessage();
   while(true)
   {
     if (digitalRead(button1) == LOW)
     {
-      return;
+      break;
     }
     
     if (digitalRead(button2) == LOW)
     {
-      hours--;
+      hour--;
       
-      if (hours == 0)
-        hours += 12;
+      if (hour == 0)
+        hour += 12;
        
-      printTime();
+      printTimeUnit(hour, 0);
       delay(300);
     }    
     
     if (digitalRead(button3) == LOW)
     {
-      hours++;
+      hour++;
 
-      if (hours > 12)
-        hours -= 12;
+      if (hour > 12)
+        hour -= 12;
 
-      printTime();
+      printTimeUnit(hour, 0);
       delay(300);
     }
   }
+
+  return hour;
 }
 
 /**
  * Allows user to change minute with buttons
  ********************************************************************************/
-void changeMinute()
+unsigned int changeMinute(unsigned int minute)
 {
   changeMinuteMessage();
   while(true)
   {
     if (digitalRead(button1) == LOW)
     {
-      return;
+      break;
     }
 
     if (digitalRead(button2) == LOW)
     {
-      minutes--;
+      minute--;
 
-      if (minutes == 0)
-        minutes = 59;
+      if (minute == 0)
+        minute = 59;
 
-      printTime();
+      printTimeUnit(minute, 3);
       delay(300);     
     }
     
     if (digitalRead(button3) == LOW)
     {
-      minutes++;
+      minute++;
 
-      if(minutes >= 60)
-        minutes = 0;
+      if(minute >= 60)
+        minute = 0;
 
-      printTime();
+      printTimeUnit(minute, 3);
       delay(300);
     }
   }
+
+  return minute;
 }
 
 /**
@@ -115,12 +120,12 @@ void changeAmPm()
   while(true)
   {
     if (digitalRead(button1) == LOW)
-      return;
+      break;
 
     if (digitalRead(button2) == LOW)
     {
       updateAmPm();
-      printTime();
+      printTime(hours, minutes, seconds, amPM);
       delay(300);
     }
   }
